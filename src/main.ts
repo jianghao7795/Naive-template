5f2578af31a41c82264a8fabe7b2c14d3c8e5615
jianghao <cadencehao@163.com>
2021-10-05 19:03:51 +0800

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 
 import { createApp } from 'vue';
-import Vant from './vants';
+// import Vant from './vants';
+import Vant from 'vant'
 import App from './App.vue';
+import 'vant/lib/index.css'
 
 const baseApp = createApp(App);
-Vant.forEach((item) => {
-	baseApp.use(item);
-});
+baseApp.use(Vant)
+// Vant.forEach((item) => {
+// 	baseApp.use(item);
+// });
 baseApp.mount('#app');
