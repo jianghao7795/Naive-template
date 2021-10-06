bb03215f75f5264d8227bf4e8e55ce87fd00f1af
jianghao <jianghao@haplox.com>
2021-10-06 12:35:36 +0800

    merge

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,12 +1,20 @@
 <template>
 	<div>
 		{{ aa }}
-		<div></div>
+		<h1>todoList</h1>
+		<ul>
+			<li v-for="() in lists"></li>
+		</ul>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from '@vue/reactivity';
+
 const aa = 'bb';
+const state = reactive({
+	lists: [1, 2, 3],
+});
 </script>
 
 <style scoped></style>
