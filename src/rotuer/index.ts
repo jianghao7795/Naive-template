c72f9318334870d3c5b005d33d5c7c46d5ef2802
jianghao <jianghao@haplox.com>
2021-10-08 17:24:32 +0800

    vue-router

--- src/rotuer/index.ts
+++ src/rotuer/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/home.vue';
-import About from '../views/about.vue';
+import Home from '@/views/home.vue';
+import About from '@/views/about.vue';
 
 const routers = [
   {
