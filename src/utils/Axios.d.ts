a1fc932cb2349f36081915c62f5f9f6162b78868
jianghao <jianghao@haplox.com>
2021-11-09 21:03:00 +0800

    API.d

--- src/utils/Axios.d.ts
+++ src/utils/Axios.d.ts
@@ -1,11 +1,5 @@
-import {MessageApiInjection} from "naive-ui/es/message/src/MessageProvider";
-import {NotificationApiInjection} from "naive-ui/es/notification/src/NotificationProvider";
-
 declare namespace API {
     export interface UserType {
         name: string;
     }
 }
-
-
-
