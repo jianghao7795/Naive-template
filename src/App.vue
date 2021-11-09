5e13a09b5f29dd4a9b1a1d0be499e737b3378262
jianghao <jianghao@haplox.com>
2021-11-09 16:22:43 +0800

    更新主题

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,60 @@
 <template>
-  <n-message-provider>
-    <n-notification-provider>
-      <router-view />
-    </n-notification-provider>
-  </n-message-provider>
+  <n-config-provider :theme="theme" :locale="zhCN" :date-locale="dateZhCN">
+    <n-switch :on-update:value="changeTheme" class="dark-theme-switch">
+      <template #checked>
+        <n-icon size="14" color="#ffd93b">
+          <Moon />
+        </n-icon>
+      </template>
+      <template #unchecked>
+        <n-icon size="14" color="#ffd93b">
+          <SunnySharp />
+        </n-icon>
+      </template>
+    </n-switch>
+    <n-message-provider>
+      <n-notification-provider>
+        <router-view />
+      </n-notification-provider>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { NMessageProvider, NNotificationProvider } from 'naive-ui';
+import { defineComponent, ref } from 'vue';
+import { NMessageProvider, NNotificationProvider, NConfigProvider, NSwitch, NIcon } from 'naive-ui';
+import { darkTheme, zhCN, dateZhCN } from 'naive-ui';
+import { BuiltInGlobalTheme } from 'naive-ui/es/themes/interface';
+import { Moon, SunnySharp } from '@vicons/ionicons5';
 export default defineComponent({
   name: 'App',
   components: {
     NMessageProvider,
     NNotificationProvider,
+    NConfigProvider,
+    NSwitch,
+    Moon,
+    SunnySharp,
+    NIcon,
   },
+
   setup() {
-    return {};
+    const theme = ref<BuiltInGlobalTheme | null>(null);
+    const changeTheme = (e: boolean) => {
+      // console.log(e);
+      if (e) {
+        theme.value = darkTheme;
+      } else {
+        theme.value = null;
+      }
+    };
+    return {
+      // darkTheme,
+      zhCN,
+      dateZhCN,
+      theme,
+      changeTheme,
+    };
   },
 });
 </script>
