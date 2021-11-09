408c01caa9640ec17dd88e7ba028f1aec731c69d
jianghao <jianghao@haplox.com>
2021-11-09 16:30:48 +0800

    正确导入less

--- src/App.vue
+++ src/App.vue
@@ -60,17 +60,5 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  overflow: hidden;
-  width: 100%;
-  position: relative;
-  min-height: 100vh;
-}
-
-.child-view {
-  position: absolute;
-  width: 100%;
-  overflow: hidden;
-  transition: all 0.5s cubic-bezier(0.88, 0, 0.1, 1);
-}
+@import './styles/global.less';
 </style>
